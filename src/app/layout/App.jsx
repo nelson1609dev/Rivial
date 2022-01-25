8fcc7786af0866e7f2a673681c634063c4a0d03f
Nelson16dev <39003986+nelson1609dev@users.noreply.github.com>
2022-01-25 15:31:23 +0530

    Issues Changed

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar';
+import { Container } from 'semantic-ui-react';
+
+export default function App() {
+  const [formOpen, setFormOpen] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
+  function handleSelectEvent(event) {
+    setSelectedEvent(event);
+    setFormOpen(true);
+  }
+
+  function handleCreateFormOpen() {
+    setSelectedEvent(null);
+    setFormOpen(true);
+  }
+
+  return (
+    <>
+      <NavBar setFormOpen={handleCreateFormOpen} />
+      <Container className='main'>
+        <EventDashboard
+          formOpen={formOpen}
+          setFormOpen={setFormOpen}
+          selectEvent={handleSelectEvent}
+          selectedEvent={selectedEvent}
+        />
+      </Container>
+    </>
+  );
+}
